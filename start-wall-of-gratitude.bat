8abc887880228e9b73e43d45c7aef0a0581ff15e
Niklas Lühr <niklas.luehr1@gmail.com>
2025-04-08 01:17:31 +0700

    change startup script to open deployed page in browser

--- start-wall-of-gratitude.bat
+++ start-wall-of-gratitude.bat
@@ -1,10 +1,2 @@
 @echo off
-cd /d "%~dp0"
-start /B npm start
-:check
-curl -s http://localhost:3000 >nul 2>&1
-if errorlevel 1 (
-    timeout /t 1 /nobreak >nul
-    goto check
-)
-start http://localhost:3000 
+start https://wog.niklasluehr.com 
