dea74bd6657960b51460e5c23a7063b55e2884a8
Niklas Lühr <niklas.luehr1@gmail.com>
2025-04-09 13:52:12 +0700

    add title and footer to start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="max-w-md mx-auto flex flex-col items-center justify-center min-h-[80vh]">
+      <header className="mb-12">
+        <h1 className="text-2xl font-bold text-center">Wall of Gratitude</h1>
+        <p className="text-center text-sm text-muted-foreground">
+          {`${new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}`}
+        </p>
+      </header>
       <GratitudeForm />
 
       <div className="mt-4">
@@ -14,6 +24,16 @@ export default function Home() {
           </Button>
         </Link>
       </div>
+      <footer className="fixed bottom-2 left-0 right-0 text-center text-xs text-gray-500">
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          className="hover:underline"
+          target="_blank"
+          href="https://niklasluehr.com"
+        >
+          niklasluehr.com
+        </a>
+      </footer>
     </div>
   );
 }
